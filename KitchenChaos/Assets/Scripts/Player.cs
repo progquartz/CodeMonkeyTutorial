a0e2ffd294d01c7e3daf69c264a29aef63584569
DESKTOP-SMTK8E3\Etherquartz <progquartz@gmail.com>
2024-01-02 16:26:43 +0900

    06 - Collision Detection - Rigidbody를 사용하지 않은 충돌 처리 이동 방식.

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -14,8 +14,44 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * Time.deltaTime * moveSpeed;
 
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 1f;
+
+
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if(!canMove)
+        {
+            // cannot move towards moveDir
+
+            // Attemt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if(canMove)
+            {
+                // move on x axis.
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // X로 못움직임.
+                // Z축 이동 시도.
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if(canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if(canMove)
+        {
+            transform.position += moveDir * Time.deltaTime * moveSpeed;
+        }
+        
         isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 10f;

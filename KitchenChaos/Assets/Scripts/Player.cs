ccf484b583421da5d73e979070972c23285d4d4b
DESKTOP-SMTK8E3\Etherquartz <progquartz@gmail.com>
2024-01-02 14:49:03 +0900

    Animation - Animation basics.

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 7f;
+    private bool isWalking;
 
     private void Update()
     {
@@ -32,8 +33,15 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * Time.deltaTime * moveSpeed;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         Debug.Log(inputVector);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }

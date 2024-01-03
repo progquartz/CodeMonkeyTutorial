0c10cc988ac290ce7c9195f001fc404addf7abea
DESKTOP-SMTK8E3\Etherquartz <progquartz@gmail.com>
2024-01-03 22:59:08 +0900

    11- player pickup, c# interface - interface basics - player pickup

--- KitchenChaos/Assets/Scripts/KitchenObject.cs
+++ KitchenChaos/Assets/Scripts/KitchenObject.cs
@@ -6,8 +6,35 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    private IKitchenObjectParent kitchenObjectParent;
+
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
+    }
+
+    public void SetKitchenObjectParent( IKitchenObjectParent kitchenObjectParent)
+    {
+        if(this.kitchenObjectParent != null) // 이전의 counter가 비어있지않다면.
+        {
+            this.kitchenObjectParent.ClearKitchenObject(); // 클리어시켜주기.
+        }
+
+        this.kitchenObjectParent = kitchenObjectParent;
+        
+        if (kitchenObjectParent.HasKitchenObject())
+        {
+            Debug.LogError("Counter already has a Kitchen Object");
+        }
+
+        kitchenObjectParent.SetKitchenObject(this);
+
+        transform.parent = kitchenObjectParent.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public IKitchenObjectParent GetKitchenObjectParent()
+    {
+        return kitchenObjectParent;
     }
 }

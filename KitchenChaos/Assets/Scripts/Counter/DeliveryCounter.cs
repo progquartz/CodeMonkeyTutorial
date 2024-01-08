640243697da392c0ac7bc2e22bcaf40e4f921e1e
DESKTOP-SMTK8E3\Etherquartz <progquartz@gmail.com>
2024-01-08 17:04:56 +0900

    23 - delivery counter, shader graph

---
+++ KitchenChaos/Assets/Scripts/Counter/DeliveryCounter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if(player.HasKitchenObject())
+        {
+            // 만약 접시일 경우만 삭제.
+            if(player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
+
